e2b24c61ddd22b824c346dc6a2ab862b02625eb1
root <root@ip-172-31-37-230.ec2.internal>
2025-03-07 20:16:36 +0000

    cambios

--- iniciardockers.sh
+++ iniciardockers.sh
@@ -1,2 +1,3 @@
 docker run --rm -it -p 8000:8000 -u $(id -u):$(id -g) -v "$PWD":/docs squidfunk/mkdocs-material new .
 docker run --rm -it -p 8000:8000 -u $(id -u):$(id -g) -v "$PWD":/docs squidfunk/mkdocs-material
+docker run --rm -it -u $(id -u):$(id -g) -v "$PWD":/docs squidfunk/mkdocs-material build
